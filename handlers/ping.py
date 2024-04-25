b7ca947ba588dfa46970cc984842e9dd17194997
ramdirt <ramdirt@mail.ru>
2024-04-25 14:29:02 +0300

    Update

--- handlers/ping.py
+++ handlers/ping.py
@@ -1,9 +1,13 @@
 from fastapi import APIRouter
+from settings import Settings
 
 router = APIRouter(prefix="/ping", tags=["ping"])
 
 @router.get("/db")
 async def ping_db():
+    settings = Settings()
+    settings.GOOGLE_TOKEN_ID
+
     return {"message": "ok"}
 
 @router.get("/app")
