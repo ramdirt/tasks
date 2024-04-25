de63668eb2e2c9752f5c4d53ea9e6ad4f5aa7954
Reznikov Aleksey <ramdirt@mail.ru>
2024-04-25 16:12:14 +0300

    Update

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 .DEFAULT_GOLAD := help
 
 run: ## start server
-	poetry run uvicorn main:app --reload
+	poetry run uvicorn main:app --host 0.0.0.0 --port 8000 --reload --env-file .local.env
 
 install:
 	poetry add $(LIBBARY)
--- handlers/ping.py
+++ handlers/ping.py
@@ -5,11 +5,14 @@ router = APIRouter(prefix="/ping", tags=["ping"])
 
 @router.get("/db")
 async def ping_db():
-    settings = Settings()
-    settings.GOOGLE_TOKEN_ID
-
     return {"message": "ok"}
 
 @router.get("/app")
 async def ping_app():
     return {"message": "ok"}
+
+
+@router.get("/token")
+async def token():
+    settings = Settings()
+    return {"message": settings.GOOGLE_TOKEN_ID}
